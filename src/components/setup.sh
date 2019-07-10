008d157a7b50c27933d0e6d1e613e0582f395e6d
cmaricle <courtneymaricle@gmail.com>
2019-07-10 16:15:56 -0700

    add setup shell script

---
+++ src/components/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ Install aws client
+echo -e "\n\n\nInstalling aws client..."
+brew install awscli
+
+# Install pip files
+echo -e "\n\n\nInstalling pip needs..."
+pip install boto3
+pip install requests
+
+# Install npm UI needs
+echo -e "\n\n\nInstalling UI-react elements via npm"
+npm install --save google-map-react
+npm install @material-ui/styles
+npm install @material-ui/core
+npm install @material-ui/icons
+
+echo -e "\n\n\n\n################\nTo finish, you'll need to configure aws (see here: https://supsystic.com/documentation/id-secret-access-key-amazon-s3/): \n\t1. aws configure, then \n\t2. python config.py"
