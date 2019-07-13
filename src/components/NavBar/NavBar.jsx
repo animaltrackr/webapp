09d5fe2d156a6d897ded1f4b04357184b8e308b4
cmaricle <courtneymaricle@gmail.com>
2019-07-13 13:17:18 -0700

    add map componnent add header bar with toggling functionality between dummy data add link to home page

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,56 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Menu, Icon, Typography } from 'antd';
+import { Link } from 'react-router-dom';
+import styles from './NavBar.css';
 
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+const { Title } = Typography;
+const { SubMenu } = Menu;
 
-const useStyles = makeStyles(theme => ({
-	root: {
-		flexGrow: 1,
-	},
-	menuButton: {
-		marginRight: theme.spacing(2),
-		color: '#21862B',
-	},
-	navBar: {
-		background: 'linear-gradient(45deg, #FFFFFF 30%, #F3F1F1 90%)',
-	},
-	title: {
-		flexGrow: 1,
-		textAlign: 'center',
-		display: 'inline-block',
-		color: '#21862B',
-	},
-	login: {
-		color: '#21862B',
-	},
-}));
-
-export default function NavBar() {
-	const classes = useStyles();
-
-	return (
-		<div className={classes.root}>
-			<AppBar position="static" className={classes.navBar}>
-				<Toolbar>
-					<IconButton
-						edge="start"
-						className={classes.menuButton}
-						color="inherit"
-						aria-label="Menu"
+class NavBar extends React.Component {
+	render() {
+		return (
+			<Menu mode="horizontal">
+				<Menu.Item>
+					<Title level={3}>
+						<Link to="/">animaltrckr</Link>
+					</Title>
+				</Menu.Item>
+				<SubMenu
+					title={<span className="submenu-title-wrapper">Active Collars</span>}
+				>
+					{this.props.deerStates.map((element, index) => {
+						return (
+							<Menu.ItemGroup title={element['name']}>
+								<Menu.Item key={index} onClick={this.props.handler}>
+									Show Tracks
+								</Menu.Item>
+							</Menu.ItemGroup>
+						);
+					})}
+					<Menu.Item
+						key={this.props.deerStates.length + 1}
+						onClick={this.props.handler}
 					>
-						<MenuIcon />
-					</IconButton>
-					<Typography variant="h6" className={classes.title}>
-						Active Tracking Collars
-					</Typography>
-					<Button className={classes.login}>Login</Button>
-				</Toolbar>
-			</AppBar>
-		</div>
-	);
+						Show All
+					</Menu.Item>
+					}
+				</SubMenu>
+			</Menu>
+		);
+	}
 }
+export default NavBar;
--- src/components/pages/Tracking/Tracking.jsx
+++ src/components/pages/Tracking/Tracking.jsx
@@ -1,18 +1,65 @@
 import React, { Component } from 'react';
 import Map from '../../Map/Map';
 import NavBar from '../../NavBar/NavBar';
+import { PropTypes } from 'prop-types';
 
 class Tracking extends Component {
+	constructor(props) {
+		super(props);
+		this.handler = this.handler.bind(this);
+		this.state = {
+			deerStates: this.props.deerStates,
+			showAll: false,
+		};
+	}
+
+	handler(e) {
+		if (e.key == this.props.deerStates.length + 1) {
+			console.log(this.state.showAll);
+
+			this.setState({ showAll: !this.state.showAll });
+			return;
+		}
+		var temp = this.props.deerStates;
+		temp[e.key].state = !temp[e.key].state;
+		this.setState({ deerStates: temp });
+	}
+
+	static defaultProps = {
+		deerStates: [
+			{
+				name: 'Deer 1',
+				id: 1,
+				state: false,
+				last_location: [[48.471, -123.324], [48.475, -123.328]],
+				colour: 'blue',
+			},
+			{
+				name: 'Deer 2',
+				id: 2,
+				state: false,
+				last_location: [
+					[48.462, -123.313],
+					[48.465, -123.318],
+					[48.469, -123.312],
+				],
+				colour: 'white',
+			},
+		],
+	};
+
 	render() {
 		return (
 			<div>
-				<NavBar />
-				<Map />
+				<NavBar deerStates={this.props.deerStates} handler={this.handler} />
+				<Map deerStates={this.props.deerStates} showAll={this.state.showAll} />
 			</div>
 		);
 	}
 }
 
-Tracking.propTypes = {};
+Tracking.propTypes = {
+	deerStates: PropTypes.instanceOf(Map).isRequired,
+};
 
 export default Tracking;
