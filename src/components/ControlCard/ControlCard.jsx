a9d3fe2b2a4996306be15f28cd50d7d3e62eeb11
Brendon Earl <bearl@uvic.ca>
2019-07-28 22:41:11 -0700

    Add selected deer & filter deer to control card

--- src/components/ControlCard/ControlCard.jsx
+++ src/components/ControlCard/ControlCard.jsx
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Menu, Typography, Card } from 'antd';
 import './ControlCard.less';
 
-import { Card } from 'antd';
+import DeerSelector from '../DeerSelector';
+import DateRangeForm from '../DateRangeForm';
+
+const { Text } = Typography;
 
 class ControlCard extends Component {
 	render() {
-		return <Card className="control-card">ControlCard</Card>;
+		return (
+			<Card className="control-card">
+				<div className="control-section">
+					<Text strong>Selected Deer</Text>
+					<DeerSelector
+						deerStates={this.props.deerStates}
+						toggleDeer={this.props.toggleDeer}
+					/>
+				</div>
+				<div className="control-section">
+					<Text strong>Filter Deer</Text>
+					<DateRangeForm
+						handleDateFilter={this.props.handleDateFilter}
+						handleDrawer={this.props.toggleDrawer}
+					/>
+				</div>
+			</Card>
+		);
 	}
 }
 
 ControlCard.propTypes = {
-	DeerStates: PropTypes.arrayOf([PropTypes.object]),
+	deerStates: PropTypes.arrayOf(PropTypes.object).isRequired,
+	toggleDeer: PropTypes.func.isRequired,
 };
 
 export default ControlCard;
