caf518afa59d3c73a19aab89763449952908e40d
Brendon Earl <bearl@uvic.ca>
2019-07-29 13:40:20 -0700

    Get tests passing (nevermind conosle warnings/logs)

--- src/components/DeerSelector/DeerSelector.test.js
+++ src/components/DeerSelector/DeerSelector.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { deerStates } from 'modules/stateMocks';
 import DeerSelector from './DeerSelector';
 
 describe('<DeerSelector />', () => {
 	test('renders', () => {
-		const wrapper = shallow(<DeerSelector />);
+		const wrapper = shallow(<DeerSelector deerStates={deerStates} />);
 		expect(wrapper).toMatchSnapshot();
 	});
 });
