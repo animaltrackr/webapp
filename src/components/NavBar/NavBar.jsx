29f4c3de6b44286c8b7c240bd752ff9f44d92de7
Brendon Earl <bearl@uvic.ca>
2019-07-28 23:13:55 -0700

    Simplify NavBar and it's styles

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,66 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import { Menu, Typography, Button } from 'antd';
+import { Typography, Button } from 'antd';
 import FilterDrawer from '../FilterDrawer/FilterDrawer';
 
 import './NavBar.less';
 
 const { Title } = Typography;
-const { SubMenu } = Menu;
 
 class NavBar extends React.Component {
 	render() {
 		return (
 			<React.Fragment>
-				<Menu mode="horizontal" className="menu-wrapper">
-					<Menu.Item>
-						<Title level={3}>
-							<Link to="/">animaltrackr</Link>
-						</Title>
-					</Menu.Item>
-					<SubMenu
-						className="submenu-wrapper"
-						title={
-							<span className="submenu-title-wrapper">Active Collars</span>
-						}
-					>
-						{this.props.deerStates
-							? this.props.deerStates.map((deer, index) => {
-									return (
-										<Menu.ItemGroup
-											key={deer.id}
-											title={deer.name}
-											className="menu-group"
-										>
-											<Menu.Item
-												key={deer.id}
-												onClick={this.props.toggleDeer}
-												className={
-													deer.visible ? 'tracker-selected' : 'tracker'
-												}
-											>
-												Show Tracks
-											</Menu.Item>
-										</Menu.ItemGroup>
-									);
-							  })
-							: null}
-						<Menu.Item
-							onClick={this.props.toggleAllDeer}
-							className={this.props.showAll ? 'tracker-selected' : 'tracker'}
-						>
-							Show All
-						</Menu.Item>
-					</SubMenu>
+				<div className="menu-wrapper">
+					<Title level={3}>
+						<Link to="/">animaltrackr</Link>
+					</Title>
 					<Button
 						onClick={this.props.toggleDrawer}
-						hidden={this.props.hideFilter}
-						style={{ left: '71%' }}
+						disabled={this.props.hideFilter}
 					>
 						Filter
 					</Button>
-				</Menu>
+				</div>
 				<FilterDrawer
 					deerStates={this.props.deerStates}
 					drawerVisible={this.props.drawerVisible}
@@ -73,4 +35,12 @@ class NavBar extends React.Component {
 		);
 	}
 }
+
+NavBar.propTypes = {
+	drawerVisible: PropTypes.bool.isRequired,
+	hideFilter: PropTypes.bool.isRequired,
+	toggleDrawer: PropTypes.func.isRequired,
+	handleDateFilter: PropTypes.func.isRequired,
+};
+
 export default NavBar;
