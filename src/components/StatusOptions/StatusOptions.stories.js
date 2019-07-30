a508b86479921dc2d985cbccc06fa4ad0f10db7f
cmaricle <courtneymaricle@gmail.com>
2019-07-30 15:30:32 -0700

    delete unused status and colour options

--- src/components/StatusOptions/StatusOptions.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import StatusOptions from './StatusOptions';
-
-storiesOf('StatusOptions', module).add('StatusOptions', () => (
-	<div>
-		<StatusOptions />
-	</div>
-));
